37d586d91c72b8c5a15531de0f99fb427b797b3b
KMM881 <kmm881@example.com>
2026-01-03 17:04:10 +0300

    تفعيل مستودعات التحميل لتجاوز خطأ Plugin not found

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
 rootProject.name = "najed-express"
 include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
